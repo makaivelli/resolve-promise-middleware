f95d53fda6c5b443638530d287b9fb2958439a3e
Adam Makai <adam.makai@gmail.com>
2018-08-21 15:26:44 +0100

    Add first test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,12 @@
-test('jest is working1', () => {
-    expect(true).toBe(true)
+import { resolvePromiseMiddleware } from '../src/index'
+
+const action = {type: 'ACTION', payload: Promise.resolve('value')};
+const fakeReduxPromiseMiddleware = jest.fn(() => Promise.resolve({
+    action: action,
+    value: 'value'
+}));
+
+test('jest should work with resolvePromiseMiddleware', () => {
+    const middleware = resolvePromiseMiddleware()(fakeReduxPromiseMiddleware)(action);
+    return expect(middleware).resolves.toBe('value')
 });
