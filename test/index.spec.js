e89ad9e5623f586394dbcb9a0f2693af53fc6156
Adam Makai <adam.makai@gmail.com>
2018-08-21 17:15:36 +0100

    Test with promiseMiddleware

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,30 +1,19 @@
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+
 import { resolvePromiseMiddleware } from '../src/index'
 
 const action = {type: 'ACTION', payload: 'payload'};
 const asyncAction = {type: 'ASYNC_ACTION', payload: Promise.resolve('value')};
 
-const fakeReduxPromiseMiddleware = jest.fn((action) => {
-    let result;
-    if (typeof action.payload.then === 'function') {
-        result = {
-            action: action,
-            value: action.payload
-        };
-    }
-    else {
-        result = action;
-    }
-    return Promise.resolve(result);
-});
-
 describe('resolvePromiseMiddleware', () => {
     test('should return the action if payload is not a promise', () => {
-        const middleware = resolvePromiseMiddleware()(fakeReduxPromiseMiddleware)(action);
-        return expect(middleware).resolves.toBe(action)
+        const store = createStore(() => ({}), applyMiddleware(resolvePromiseMiddleware, promiseMiddleware()));
+        return expect(store.dispatch(action)).toEqual(action);
     });
 
     test('should return the value if payload is a promise', () => {
-        const middleware = resolvePromiseMiddleware()(fakeReduxPromiseMiddleware)(asyncAction);
-        return expect(middleware).resolves.toBe('value')
+        const store = createStore(() => ({}), applyMiddleware(resolvePromiseMiddleware, promiseMiddleware()));
+        return expect(store.dispatch(asyncAction)).resolves.toBe('value');
     });
 });
