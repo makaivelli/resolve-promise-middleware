cc136811ce8b193cefe0dd9486012e59cbc23065
Adam Makai <adam.makai@gmail.com>
2018-08-21 16:00:06 +0100

    Add test for non-promise payload

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,30 @@
 import { resolvePromiseMiddleware } from '../src/index'
 
-const action = {type: 'ACTION', payload: Promise.resolve('value')};
-const fakeReduxPromiseMiddleware = jest.fn(() => Promise.resolve({
-    action: action,
-    value: 'value'
-}));
+const action = {type: 'ACTION', payload: 'payload'};
+const asyncAction = {type: 'ASYNC_ACTION', payload: Promise.resolve('value')};
 
-test('jest should work with resolvePromiseMiddleware', () => {
-    const middleware = resolvePromiseMiddleware()(fakeReduxPromiseMiddleware)(action);
-    return expect(middleware).resolves.toBe('value')
+const fakeReduxPromiseMiddleware = jest.fn((action) => {
+    let result;
+    if (typeof action.payload.then === 'function') {
+        result = {
+            action: action,
+            value: action.payload
+        };
+    }
+    else {
+        result = action;
+    }
+    return Promise.resolve(result);
+});
+
+describe('resolvePromiseMiddleware', () => {
+    test('should return the action if payload is not a promise', () => {
+        const middleware = resolvePromiseMiddleware()(fakeReduxPromiseMiddleware)(action);
+        return expect(middleware).resolves.toBe(action)
+    });
+
+    test('should return the value if payload is a promise', () => {
+        const middleware = resolvePromiseMiddleware()(fakeReduxPromiseMiddleware)(asyncAction);
+        return expect(middleware).resolves.toBe('value')
+    });
 });
