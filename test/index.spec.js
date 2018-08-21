403da6ae7cbbe13c70796c79b02a6e77572c8639
Adam Makai <adam.makai@gmail.com>
2018-08-21 17:32:21 +0100

    Add test for passing through next

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,17 +3,28 @@ import promiseMiddleware from 'redux-promise-middleware';
 
 import { resolvePromiseMiddleware } from '../src/index'
 
-const action = {type: 'ACTION', payload: 'payload'};
-const asyncAction = {type: 'ASYNC_ACTION', payload: Promise.resolve('value')};
-
 describe('resolvePromiseMiddleware', () => {
-    test('should return the action if payload is not a promise', () => {
-        const store = createStore(() => ({}), applyMiddleware(resolvePromiseMiddleware, promiseMiddleware()));
-        return expect(store.dispatch(action)).toEqual(action);
+    test('should pass the action to the next middleware', () => {
+        const action = {type: 'ACTION', payload: 'payload'};
+        const fakeNext = jest.fn();
+
+        resolvePromiseMiddleware()(fakeNext)(action);
+        expect(fakeNext).toHaveBeenCalledWith(action);
     });
 
-    test('should return the value if payload is a promise', () => {
-        const store = createStore(() => ({}), applyMiddleware(resolvePromiseMiddleware, promiseMiddleware()));
-        return expect(store.dispatch(asyncAction)).resolves.toBe('value');
+    describe('applied before promiseMiddleware', () => {
+        test('should return the action if payload is not a promise', () => {
+            const store = createStore(() => ({}), applyMiddleware(resolvePromiseMiddleware, promiseMiddleware()));
+            const action = {type: 'ACTION', payload: 'payload'};
+
+            return expect(store.dispatch(action)).toEqual(action);
+        });
+
+        test('should return the value if payload is a promise', () => {
+            const store = createStore(() => ({}), applyMiddleware(resolvePromiseMiddleware, promiseMiddleware()));
+            const action = {type: 'ASYNC_ACTION', payload: Promise.resolve('value')};
+
+            return expect(store.dispatch(action)).resolves.toBe('value');
+        });
     });
 });
